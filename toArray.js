f51f20ce2b7c7af53de96ea7771b1b761585b2ce
b-price <bprice12@students.towson.edu>
2024-03-22 23:22:37 -0400

    toArray completed

---
+++ toArray.js
@@ -0,0 +1,13 @@
+var count = 0
+var nums = []
+process.argv.forEach(evenNums)
+
+function evenNums(item, index) {
+    if (index > 1 && item % 2 === 0) {
+        nums[count] = item
+        count++
+    }
+}
+
+console.log("The array contains: " + nums)
+console.log("The length of the array is: " + nums.length)
